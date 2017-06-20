2c2cbd067eef9af24085225e7817db52cf5e697a
nrkroeker <nathaliekroeker@gmail.com>
2017-06-20 12:13:01 -0400

    import user data

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+  state = {
+    user: {
+      avatar_url: '',
+      login: '',
+      followers: '',
+      following: '',
+      location: '',
+      html_url: ''
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.fetchUserData()
+
+  }
+
+  fetchUserData = () => {
+    fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+      .then(response => response.json())
+      .then(user => this.setState({ user }))
+  }
+
   render() {
+    const { user } = this.state
     return(
-      
+      <div className="user">
+        <img src={user.avatar_url} alt="user" />
+        <h2>{user.login}</h2>
+        <h3>Followers: {user.followers}</h3>
+        <h3>Following: {user.following}</h3>
+        <h3>Location: {user.location}</h3>
+        <a href={user.html_url} target="_">Link to {user.login}{"'"}s profile</a>
+      </div>
     )
   }
 }
